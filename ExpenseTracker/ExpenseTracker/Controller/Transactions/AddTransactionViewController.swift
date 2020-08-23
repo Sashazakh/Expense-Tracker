47c6996835c4cc2853596a7a9b180b6b3cca484d
Alexandr <alexanzakharov@gmail.com>
2020-08-23 12:00:15 +0300

    Added Detail Tf and keyboard for it

--- ExpenseTracker/ExpenseTracker/Controller/Transactions/AddTransactionViewController.swift
+++ ExpenseTracker/ExpenseTracker/Controller/Transactions/AddTransactionViewController.swift
@@ -33,6 +33,14 @@ class AddTransactionViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
+    @IBOutlet weak var detailTF: UIStackView!
+    {
+        didSet
+        {
+            amountTF.delegate = self
+        }
+    }
+    
     private var datePicker: UIDatePicker?
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,9 +62,20 @@ class AddTransactionViewController: UIViewController, UITextFieldDelegate {
         let tapScreen = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
         view.addGestureRecognizer(tapScreen)
         
-         incomeView.backgroundColor = #colorLiteral(red: 0, green: 0.3921568627, blue: 0, alpha: 0.6977739726)
+        incomeView.backgroundColor = #colorLiteral(red: 0, green: 0.3921568627, blue: 0, alpha: 0.6977739726)
+//
+//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-   
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.addKeyboardObserver()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.removeKeyboardObserver()
+    }
     
     @objc func viewTapped()
     {
@@ -128,5 +147,66 @@ class AddTransactionViewController: UIViewController, UITextFieldDelegate {
         currentType = .income
         incomeView.backgroundColor = #colorLiteral(red: 0, green: 0.3921568627, blue: 0, alpha: 0.6977739726)
         expenseView.backgroundColor = #colorLiteral(red: 0.3678154181, green: 0.3678154181, blue: 0.3678154181, alpha: 1)
+    }
+}
+
+
+extension UIViewController {
+    func addKeyboardObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardNotifications(notification:)),
+                                               name: UIResponder.keyboardWillChangeFrameNotification,
+                                               object: nil)
+    }
+    
+    func removeKeyboardObserver(){
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+
+    @objc func keyboardNotifications(notification: NSNotification) {
+        
+        var tfY : CGFloat = 0.0
+        let spaceBetweenTxtFieldAndKeyboard : CGFloat = 20
+        
+        
+        var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+        if let activeTextField = UIResponder.currentFirst() as? UITextField ?? UIResponder.currentFirst() as? UITextView {
+            frame = self.view.convert(activeTextField.frame, from:activeTextField.superview)
+            tfY = frame.origin.y + frame.size.height
+        }
+        
+        if let userInfo = notification.userInfo {
+            let keyBoardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+            let keyBoardFrameY = keyBoardFrame!.origin.y
+            let keyBoardFrameHeight = keyBoardFrame!.size.height
+            
+            var viewOriginY: CGFloat = 0.0
+
+            if keyBoardFrameY >= UIScreen.main.bounds.size.height {
+                viewOriginY = 0.0
+            } else {
+                if tfY >= keyBoardFrameY {
+                    
+                    viewOriginY = (tfY - keyBoardFrameY) + spaceBetweenTxtFieldAndKeyboard
+                    if viewOriginY > keyBoardFrameHeight { viewOriginY = keyBoardFrameHeight }
+                }
+            }
+            
+            self.view.frame.origin.y = -viewOriginY
+        }
+    }
+}
+
+extension UIResponder {
+    
+    static weak var responder: UIResponder?
+    
+    static func currentFirst() -> UIResponder? {
+        responder = nil
+        UIApplication.shared.sendAction(#selector(trap), to: nil, from: nil, for: nil)
+        return responder
+    }
+    
+    @objc private func trap() {
+        UIResponder.responder = self
     }
 }
